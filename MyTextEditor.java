1eaafcf10f16f4d3b4714dd3db861af8f9212c3e
GreenSkies <abhinav.interact1997@gmail.com>
2016-03-15 12:21:20 -0700

    Functions class, you may have to fix a few small things, also replace needs to highlight syntax in another way, I can probably do that later

--- MyTextEditor.java
+++ MyTextEditor.java
@@ -6,12 +6,8 @@ import javax.swing.event.DocumentEvent;
 import java.util.Scanner;
 import java.io.*;
 import javax.swing.text.BadLocationException;
-import javax.swing.text.DefaultHighlighter;
-import javax.swing.text.Highlighter;
-import javax.swing.text.Highlighter.HighlightPainter;
 import javax.swing.text.JTextComponent;
 import javax.swing.text.*;
-import java.net.URI;
 import java.util.*;
 
 public class MyTextEditor extends JFrame implements ActionListener
@@ -19,12 +15,13 @@ public class MyTextEditor extends JFrame implements ActionListener
    private JPanel panel = new JPanel(new BorderLayout());
    private JTextPane textArea = new JTextPane();
    private JTextArea lineText = new JTextArea();
+   private Functions functionChooser = new Functions();
    // private TextLineNumber tln = new TextLineNumber(textArea);
    private static final Color TA_BKGRD_CL = Color.BLACK;
    private static final Color TA_FRGRD_CL = Color.GREEN;
    private static final Color TA_CARET_CL = Color.WHITE;
 
-   private String extOfFile;
+   //private String extOfFile;
    private JScrollPane scrollPane;
    private MenuBar menuBar = new MenuBar(); 
    private Menu file = new Menu();
@@ -50,9 +47,6 @@ public class MyTextEditor extends JFrame implements ActionListener
    private MenuItem documentation = new MenuItem();
    //Secret Commands 
    private MenuItem delete = new MenuItem();
-   
-   private static String OS = System.getProperty("os.name").toLowerCase();
-
 
    public MyTextEditor()
    {
@@ -189,50 +183,50 @@ public class MyTextEditor extends JFrame implements ActionListener
       }
       else if(event.getSource() == this.openFile)
       {
-         openFile();
+         functionChooser.openFile(textArea);
       }
       else if(event.getSource() == this.saveFile)
       {
-         saveFile();
+         functionChooser.saveFile(textArea);
       }
       else if(event.getSource() == this.newFile)
       {
-         newFile();
+         functionChooser.newFile(textArea);
       }
       else if(event.getSource() == this.search)
       {
-         search();
+         functionChooser.search(textArea);
       }
       else if(event.getSource() == this.replace)
       {
-         replace();
-         try{
-         findKeyWords(extOfFile);
+         functionChooser.replace(textArea);
+        /* try{
+         functionChooser.findKeyWords(extOfFile, textArea);
          throw new FileNotFoundException();
          }catch(FileNotFoundException ex)
          {
 
-         }
+         }*/
       }
       else if(event.getSource() == this.goToLine)
       {
-         goToLine();
+         functionChooser.goToLine(textArea);
       }
       else if(event.getSource() == this.copy)
       {
-         copyArea();
+         functionChooser.copyArea(textArea);
       }
       else if(event.getSource() == this.paste)
       {
-         pasteArea();
+         functionChooser.pasteArea(textArea);
       }
       else if(event.getSource() == this.cut)
       {
-         cutArea();
+         functionChooser.cutArea(textArea);
       }
       else if(event.getSource() == this.documentation)
       {
-         loadJavaDocs();
+         functionChooser.loadJavaDocs();
       }
       else if(event.getSource() == this.delete)
       {
@@ -240,423 +234,7 @@ public class MyTextEditor extends JFrame implements ActionListener
          dispose();
       }
    }
-   
-   public int findIndexOfWord(String string)
-   {
-      char[] myChar = string.toCharArray();
-      char[] myChar2 = new char[myChar.length];
-      int m = 0;
-      for(int i = myChar.length - 1; i >= 0; i--)
-      {
-         myChar2[m] = myChar[i];
-         m++;
-      }
-      int z = -1;
-      for(char k : myChar2)
-      {
-
-         if(k == '.')
-         {
-            return myChar.length - z;
-         }
-         z++;
-      }
-      return -1;
-
-   }
-   public void openFile()
-   {
-      JFileChooser open = new JFileChooser();
-      int option = open.showOpenDialog(this);
-      String filename = open.getSelectedFile().getName();
-      //int indexOfWord = filename.indexOf(".");
-      int indexOfWord = findIndexOfWord(filename);
-      String ext = filename.substring(indexOfWord - 1,filename.length());
-      System.out.println(ext);
-      extOfFile = ext;
-      int total = 0;
-      if(option == JFileChooser.APPROVE_OPTION)
-      {
-         this.textArea.setText("");
-         try
-         {
-            StyledDocument doc = textArea.getStyledDocument();
-            File file = new File(filename);
-            Scanner scanner = new Scanner(new FileReader(open.getSelectedFile().getPath()));
-            while(scanner.hasNext())
-            {
-               String m = scanner.nextLine();
-               doc.insertString(total,m + "\n",null);
-               total += m.length() + 1;
-            }
-         }
-         catch(Exception ex)
-         {
-            System.out.println(ex.getMessage());
-         }
-      }
-     
-      try
-         {
-         findKeyWords(ext);
-         throw new FileNotFoundException();
-         }catch(FileNotFoundException ex)
-         {
-
-         }
-     
-   }
-   public void saveFile()
-   {
-      JFileChooser save = new JFileChooser();
-      int option = save.showSaveDialog(this);
-      if(option == JFileChooser.APPROVE_OPTION)
-      {
-         try
-            {
-            BufferedWriter output = new BufferedWriter(new FileWriter(save.getSelectedFile().getPath()));
-            output.write(this.textArea.getText());
-            output.close();
-         }
-         catch(Exception ex)
-         {
-            System.out.println(ex.getMessage());
-         }
-      }
-   }
-   public void newFile()
-   {
-      JFileChooser newF = new JFileChooser();
-      int option = newF.showSaveDialog(this);
-      if(option == JFileChooser.APPROVE_OPTION)
-      {
-         try
-         {
-            BufferedWriter output = new BufferedWriter(new FileWriter(newF.getSelectedFile().getPath()));
-            output.write(this.textArea.getText());
-            output.close();
-            this.textArea.setText("");
-         }
-         catch(Exception ex)
-         {
-            System.out.println(ex.getMessage());
-         }
-      }
-   
-   }
-   public void findKeyWords(String directory) throws FileNotFoundException
-   {
-      final StyleContext cont = StyleContext.getDefaultStyleContext();
-      final AttributeSet jKeyWord = cont.addAttribute(cont.getEmptySet(), 
-         StyleConstants.Foreground,Color.RED);
-      final AttributeSet jOperator = cont.addAttribute(cont.getEmptySet(), 
-         StyleConstants.Foreground,Color.MAGENTA);
-      final AttributeSet jtypes = cont.addAttribute(cont.getEmptySet(), 
-         StyleConstants.Foreground,Color.CYAN);
-
-         ArrayList<String> words = loadKeyWords(directory);
-         for (String line : words)
-         {
-            searchJava(line,jKeyWord);
-         }
-         ArrayList<String> operators = loadOperators(directory);
-         for (String line : operators)
-         {
-            searchJava(line, jOperator);
-         }
-         ArrayList<String> types1 = loadTypes(directory);
-         for (String line : types1)
-         {
-            searchJava(line, jtypes);
-         }
-   }
-   private ArrayList<String> loadKeyWords(String directory) throws FileNotFoundException
-   {
-      ArrayList<String> javaWords = new ArrayList<String>();
-      final String dir = System.getProperty("user.dir");
-      File file = new File(dir + "/" + directory + "/keywords.txt");
-      Scanner scan = new Scanner(file);
-      while(scan.hasNext())
-      {
-         javaWords.add(scan.next() + " ");
-      }
-      scan.close();
-      return javaWords;
-   }
-   private ArrayList<String> loadOperators(String directory) throws FileNotFoundException
-   {
-      ArrayList<String> javaWords = new ArrayList<String>();
-      final String dir = System.getProperty("user.dir");
-      File file = new File(dir + "/" + directory + "/operators.txt");
-      Scanner scan = new Scanner(file);
-      while(scan.hasNext())
-      {
-         javaWords.add(scan.next());
-      }
-      scan.close();
-      return javaWords;
-   }
-   private ArrayList<String> loadTypes(String directory) throws FileNotFoundException
-   {
-      ArrayList<String> javaWords = new ArrayList<String>();
-      final String dir = System.getProperty("user.dir");
-      File file = new File(dir + "/" + directory + "/types.txt");
-      Scanner scan = new Scanner(file);
-      while(scan.hasNext())
-      {
-         javaWords.add(" " + scan.next());
-      }
-      scan.close();
-      return javaWords;
-   }
-    public void searchJava(String wordToSearch, AttributeSet javaAttr) 
-   { 
-      
-      final AttributeSet attr = javaAttr;
-      Document text = textArea.getDocument();
-
-      int m;
-      int t;
-      int total = 0;
-      for (String line : textArea.getText().split("\n")) 
-      {
-         m = line.indexOf(wordToSearch);
-         if(m == -1)
-         {
-            if(isUnix())
-            {
-            total += line.length() + 1;
-            }
-            else if(isWindows())
-            {
-               total += line.length();
-            }
-            else if(isMac())
-            {
-               total += line.length() + 1;
-            }
-            else
-            {
-               total += line.length() + 1;
-            }
-            continue;
-         }
-         try{
-         text.remove(total + m, wordToSearch.length());
-
-         text.insertString(total + m, wordToSearch, attr);
-         }catch(BadLocationException ex)
-         {}
-         while(true)
-         {
-            m = line.indexOf(wordToSearch, m + 1 );
-
-            if (m == -1)
-            {
-               break;
-            }
-            try
-            {
-            text.remove(total + m, wordToSearch.length());
-
-            text.insertString(total + m, wordToSearch, attr);
-            }catch(BadLocationException e)
-            {
-            }
-         }
-         if(isUnix())
-            {
-               total += line.length() + 1;
-               
-            }
-            else if(isWindows())
-            {
-               total += line.length();
-            }
-            else if(isMac())
-            {
-               total += line.length() + 1;
-            }
-            else
-            {
-               JOptionPane.showMessageDialog(null, "Eric You Troll" );
-               total += line.length() + 1;
-            }
-      }
-   }
-   public void search() 
-   { 
-      String wordToSearch = JOptionPane.showInputDialog(null, "Word to search for:");
-      Highlighter highlighter = textArea.getHighlighter();
-      highlighter.removeAllHighlights();
-      HighlightPainter painter = new DefaultHighlighter.DefaultHighlightPainter(Color.pink);
-     
-      int m;
-      int total = 0;
-      for (String line : textArea.getText().split("\n")) 
-      {
-         m = line.indexOf(wordToSearch);
-         if(m == -1)
-         {
-            if(isUnix())
-            {
-            total += line.length() + 1;
-            }
-            else if(isWindows())
-            {
-               total += line.length();
-            }
-            else if(isMac())
-            {
-               total += line.length() + 1;
-            }
-            else
-            {
-               total += line.length() + 1;
-            }
-            continue;
-         }
-         try{
-         highlighter.addHighlight(total + m, total + m + wordToSearch.length(), painter);
-         }catch(BadLocationException ex)
-         {}
-         while(true)
-         {
-            m = line.indexOf(wordToSearch, m + 1);
-
-            if (m == -1)
-            {
-               
-               break;
-            }
-            try{
-            highlighter.addHighlight(total + m, total + m + wordToSearch.length(), painter);
-            }catch(BadLocationException e)
-            {
-            }
-         }
-         if(isUnix())
-            {
-            total += line.length() + 1;
-            }
-            else if(isWindows())
-            {
-               total += line.length();
-            }
-            else if(isMac())
-            {
-               total += line.length() + 1;
-            }
-            else
-            {
-               total += line.length() + 1;
-            }
-            continue;
-      }
-   }
-   public void replace()
-   {
-      String wordToSearch = JOptionPane.showInputDialog(null, "Word to replace:");
-      String wordToReplace = JOptionPane.showInputDialog(null, "Replacement word:");
-
-      int m;
-      int total = 0;
-      int wordLength = wordToSearch.length();
-      for (String line : textArea.getText().split("\n")) 
-      {
-         m = line.indexOf(wordToSearch);
-         if(m == -1)
-         {
-            total += line.length() + 1; 
-            continue;
-         }
-         
-         String newLine = line.replaceAll(wordToSearch, wordToReplace);
-         Document text = textArea.getDocument();
-         try
-         {
-         text.remove(total, line.length());
-         text.insertString(total, newLine, null);
-         }
-         catch(BadLocationException ex)
-         {
-
-         }
-         total += newLine.length() + 1;
-      }
-   }
-   public void goToLine()
-   {
-       String lineToSearch = JOptionPane.showInputDialog(null, "Line Number: ");
-       int total = 0;
-       int lineNum = 0;
-       for (String line : textArea.getText().split("\n"))
-       {
-         lineNum +=1;
-         String lineNumStr = lineNum + "";
-         if (lineNumStr.compareTo(lineToSearch) == 0)
-         {
-            if( line == null)
-            {
-               textArea.setCaretPosition(total);
-            }
-            textArea.setCaretPosition(total);
-            break;
-         }
-         total += line.length() + 1;
-
-       }
-   }
-   public void copyArea()
-   {
-      textArea.copy();
-   }
-   public void pasteArea()
-   {
-      textArea.paste();
-   }
-   public void cutArea()
-   {
-      textArea.cut();
-   }
-   public void loadJavaDocs()
-   {
-      try
-      {
-         Desktop desktop = Desktop.getDesktop();
-         URI uRL = new URI("http://users.csc.calpoly.edu/~dtimokhi/");
-         desktop.browse(uRL);
-      }
-      catch(Exception ex)
-      {
-         ex.printStackTrace();
-      }
-   }
-   public static boolean isWindows() 
-   {
-
-      return (OS.indexOf("win") >= 0);
-
-   }
-   public static boolean isLinux() 
-   {
-
-      return (OS.indexOf("Lin") >= 0);
-
-   }
-   public static boolean isUnix() {
-
-      return (OS.indexOf("nix") >= 0 ||
-       OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
-      
-   }
-   public static boolean isMac() {
-
-      return (OS.indexOf("mac") >= 0);
-
-   }
-
-   public static void main(String args[])
+   public static void main(String[] args)
    {
       MyTextEditor textEditor = new MyTextEditor();
       textEditor.setVisible(true);
